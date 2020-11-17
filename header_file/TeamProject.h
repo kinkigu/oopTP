5ea5eda38cc201b0da7aeda37e70b6daed29d0ff
JIN <63086170+kjh-icnl@users.noreply.github.com>
2020-11-17 22:42:58 +0900

    Add files via upload

---
+++ header_file/TeamProject.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+
+#ifndef __oopTP__
+#define __oopTp__
+
+class MemoList {
+	//std::vector<std::experimental::filesystem::v1::path> wholePath;
+	std::vector<std::string> wholePath;
+	std::vector<std::string> fileList;
+	std::map<int, std::string> pathType;
+
+public:
+	std::string defaultFileName = "\0";
+	std::string defaultPath = "C:/Users/USER/Desktop/Test_Folder";
+
+public:
+	// Constructor
+	MemoList();
+	// Destructor
+	~MemoList();
+
+	// Functions
+	bool OpenList();
+	bool MakeFile(std::string filename);
+	void ChangeDefaultFileName(std::string input_default);
+
+public:
+	template <typename Route>
+	bool ChangeDefaultRoute();
+
+private:
+
+
+public:
+
+};
+
+
+#endif // !oopTP
+
